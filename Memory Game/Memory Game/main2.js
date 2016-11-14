286790de9caf15c4899b2d9747863cf56982ef4d
Carter Kessler <rckessler84@yahoo.com>
2016-11-14 08:59:51 -0600

    Add files via upload
    Memory game files

---
+++ Memory Game/Memory Game/main2.js
@@ -0,0 +1,46 @@
+main.js
+
+alert('Hello, friends.');
+
+val x="queen"
+
+val y="king"
+
+var cardOne="queen"
+
+var cardTwo
+
+var cardThree
+
+var cardFour
+
+
+	
+
+
+if var x 
+
+var sayHello = function(name){
+  return "Hello, " + name + ".";
+};
+var areBothEven = function(a, b){
+  return a%2 === 0 && b%2 === 0;
+};
+var hotOrNot = function(temp){
+  return (temp > 75)? "hot" : "not hot";
+  // using an `if` statement is also acceptable
+};
+var threeIfNull = function(num){
+  if(num===null) return 3
+  return num
+};
+var greatest = function(a, b, c){
+  greatest = a;
+  if (greatest < b) {
+    greatest = b;
+  }
+  if (greatest < c) {
+    greatest = c;
+  }
+  return greatest;
+};
---
+++ Memory Game/main.js
@@ -0,0 +1,12 @@
+main.js
+
+var suit
+
+var size
+
+var location
+
+var visibility
+
+var cardOne ="queen";
+var cardTwo ="king";
