99d60201c3aeaf0949526368829a21e7c2db28ac
Carter Kessler <rckessler84@yahoo.com>
2017-01-17 10:25:11 -0600

    some updates

--- Memory Game/main.js
+++ Memory Game/main.js
@@ -12,6 +12,20 @@ var cardTwo ="king";
 var cardThree ="queen";
 var cardFour ="king";
 
-if (cardTwo === cardFour) {
-  alert("You found a match!")
+// if (cardTwo === cardFour) {
+//   alert("You found a match!")
+// };
+var gameBoard = document.getElementById('game-board');
+
+function card() {
+   gameBoard.appendChild('game-board');
 };
+function createCard(){
+  for (i = 0; i<4; i++){
+    var createCard = document.createElement('div');
+
+    // .className('card').appendChild('game-board')
+  }
+};
+
+createCard(4);
