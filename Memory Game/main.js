d17d4ddd2b9e2a8c854a7fd7d7d85c6d3fe89d4f
Carter Kessler <rckessler84@yahoo.com>
2017-01-27 16:35:29 -0600

    made it work!

--- Memory Game/main.js
+++ Memory Game/main.js
@@ -4,43 +4,50 @@
 
 
 //created array to house the cards that are clicked on
-var cardsInPlay = [];
+var cardsInPlay = [ ];
 //created array to house all the cards in play
 var cards = ["king", "queen", "king", "queen"];
+//
+// cardOne = "queen";
+// cardTwo = "king";
+// cardThree = "queen";
+// cardFour = "king";
 
-var turnCard = function(e){
-  console.log(e.target.id);
-  // document.getElementbyId(e.target.id).src = "my_queen.png";
-   if (e.target.id == "0" || e.target.id == "2") {
-     e.target.innerHTML= '<img src="my_king.png" alt="King "   />'
-   } else {
-     e.target.innerHTML='<img src="my_queen.png" alt="Queen " />'
-   }
-  };
-//wrote function
-var isTwoCards = function (e){
-   cardsInPlay.push(this.getAttribute('data-card'));
-   console.log('isTwoCards');
 
-  //  if(cardsInPlay.length===1){
-  //    isMatch(cardsInPlay);
-  //    cardsInPlay = [];
+// wrote function
+// var isMatch = function (e) {
+//
+//   if (e.target.id === cardsInPlay[0]) {
+//     return "ITS A MATCH"
+//   } else {
+//     return "It's a match!!"
+//   }
+//   console.log('isMatch');
+// };
 
- };
+var isMatch = function (e) {
+  console.log(cardsInPlay[0]);
+  console.log(cardsInPlay[1]);
+  console.log(cardsInPlay[0] === cardsInPlay[1]);
+  if (cardsInPlay[1] === cardsInPlay[0]) {
+    alert ("ITS A MATCH")
+  } else {
+    alert ("keep trying!")
+  }
+
+};
+
+
+
+
  //wrote another function
- // var isMatch = function (cardsInPlay) {
- //   console.log('isMatch');
- //   if () {
- //     return "keep trying"
- //   } else {
- //     return "Awesome bruh"
- //   }
- // };
+
 
 var createBoard = function(cards){
    for (i=0; i<cards.length; i++){
-
+    //  cardsInPlay[i] = i * i;
      //made a new elemet- div
+     var board = document.getElementById("game-board")
      var div = document.createElement("div");
      //make a text node
      var t = document.createTextNode("Memory");
@@ -55,47 +62,47 @@ var createBoard = function(cards){
      //add the div to the body, the game-board id
      document.getElementById("game-board").appendChild(div);
 
+     var turnCard = function(e){
+       console.log(e.target.id);
+       // document.getElementbyId(e.target.id).src = "my_queen.png";
+        if (e.target.id == "0" || e.target.id == "2") {
+          e.target.innerHTML= '<img src="my_king.png" alt="King "   />'
+        } else {
+          e.target.innerHTML='<img src="my_queen.png" alt="Queen " />'
+        }
+       };
+
+
+
+     var isTwoCards = function (e){
+
+        cardsInPlay.push(this.getAttribute('data-card'));
+        console.log(cardsInPlay);
+
+        if (cardsInPlay.length === 2){
+          isMatch(cardsInPlay);
+        }
+       //  else if (cardsInPlay.length === 1){
+       //    return "Find the match"
+       //  }
+       //    cardsInPlay= [];
+       //  }
+        console.log('isTwoCards');
+
+       };
+
+
+
+    //  div.addEventListener('click', isMatch);
      div.addEventListener('click', turnCard);
      div.addEventListener('click', isTwoCards);
-    //  div.addEventListener('click', isMatch);
-
-
   };
+
 };
 
 createBoard (cards);
 
-// if (cards[i]==="queen") {
-//   document.getElementbyId(cards[i]).setAttribute("img","my_queen.png" );}
-// //   document.getElementbyId("e.target.id").src = "my_queen.png";
-// // }
-// else if (e.target.id ==="king" ){
-//   document.getElementById("e.target.id").src = "my_king.png";
-//  }
 
-// if (card[i] === "queen") {
-//   div.innerHTML = '<img src="my_queen.png" alt="Queen " />';
-// }
-// else if (card[i] === "king"){
-//   div.innerHTML = '<img src="my_king.png" alt="King " />';
-//  }
-
-// document.getElementsByClassName(".card").style.background-color = "blue";
-// var x = document.querySelector(".card img");
-// x.style.visibilty = "visible";
-// a[target]{
-//   visiblilty: "visible"
-// };
- // div.style.visibilty = visible;
-//  document.getElementById(e.target.id).style.visiblity = "visible";
- //  e.target.style.visibility = "visible";
-
- //  div.innerHTML = '<img src="my_queen.png" alt="Queen " />';
-  // console.log(e);
-  // console.log(e.target);
-
-      //  document.getElementsByClassName("card").onclick = turnCard;
-      //  cards[i].addEventListener('click', turnCard);
 
 
       // var today = new Date();
@@ -115,4 +122,3 @@ createBoard (cards);
       // document.write('<h3>' + greeting + '</h3>');
 
       //  div.setAttribute('id', cards[i]);
-      //  fish.setAttribute('id', cards[1]);
