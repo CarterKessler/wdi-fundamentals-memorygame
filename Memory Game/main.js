6b667c0ce809c86469d1a7417d99d424dcf0ea52
Carter Kessler <rckessler84@yahoo.com>
2017-02-03 09:18:30 -0600

    added jquery to make paris disappear

--- Memory Game/main.js
+++ Memory Game/main.js
@@ -1,21 +1,25 @@
-//created array to house the cards that are clicked on
-var cardsInPlay = [ ];
-//created array to house all the cards in play
 var cards = ["jack", "king", "jack", "queen", "king", "queen"];
+var cardsInPlay = [ ];
 
-
-
-// resetCards(e);
+var deletePair = function(e) {
+  console.log(cardsInPlay[0]);
+  if (cardsInPlay[0]==="jack"){
+    $("div").remove(".jack");
+  } else if (cardsInPlay[0]==="queen"){
+    $("div").remove(".queen");
+  } else if (cardsInPlay[0]==="king"){
+    $("div").remove(".king");
+  } else {
+    console.log("french fries");
+  }
+};
 
 var createBoard = function(cards){
    for (i=0; i<cards.length; i++){
      //made a new elemet- div
      var board = document.getElementById("game-board")
      var div = document.createElement("div");
-     //make a text node
-    //  var t = document.createTextNode("Memory");
-     //gave a class to teh new div elements
-     div.className = "card";
+     div.className = "card "+ cards[i] ;
      //put textnode inside the div
      div.setAttribute('data-card', cards[i]);
      //added the new text to the new divs(cards)
@@ -25,10 +29,9 @@ var createBoard = function(cards){
      //add the div to the body, the game-board id
      document.getElementById("game-board").appendChild(div);
 
-
-
      var turnCard = function(e){
        console.log(e.target.id);
+       console.log(e.target.className);
         if (e.target.id == "1" || e.target.id == "4") {
           e.target.innerHTML= '<img src="my_king.png" alt="King "   />'
         } else if (e.target.id == "3" || e.target.id == "5") {
@@ -37,35 +40,25 @@ var createBoard = function(cards){
           e.target.innerHTML='<img src="jack.images.png" alt="Jack" />'
         }
        };
-      var isMatch = function (e) {
 
+    var isMatch = function (e) {
          console.log(cardsInPlay[0]);
          console.log(cardsInPlay[1]);
          console.log(cardsInPlay[0] === cardsInPlay[1]);
 
          if (cardsInPlay[0] === cardsInPlay[1]) {
-           alert ("ITS A MATCH")
+           // add "ITS A MATCH" into the text of the page
+           document.getElementById("game-text").innerHTML= "ITS A MATCH";
+           deletePair(e);
          } else {
-           alert ("try again"), resetCards(e)
+           document.getElementById("game-text").innerHTML= "Keep going", resetCards(e)
          }
          cardsInPlay = [];
-       };
-      //    } else if (cardsInPlay[1] ===cardsInPlay[2]){
-      //      alert ("Now it's a match")
-      //    } else if(cardsInPlay[1] != cardsInPlay[2]){
-      //      alert ("keep trying"), resetCards(e)
-      //    } else if (cardsInPlay[2] === cardsInPlay[3] || cardsInPlay[0] === cardsInPlay[2]){
-      //      alert ("You got it, now it's a match")
-      //    } else {
-      //      alert ("try again"), resetCards(e)
-      //    }
-      //  };
+         };
 
     var isTwoCards = function (e){
-
         cardsInPlay.push(this.getAttribute('data-card'));
         console.log(cardsInPlay);
-
         if (cardsInPlay.length === 2){
           isMatch(cardsInPlay);
         } else {
@@ -79,41 +72,12 @@ var createBoard = function(cards){
       for (var i=0; i<cardsInPlay.length; i++){
           el[i].style.visibility='hidden'
         };
-    };
+      };
 
-
-
-    // var resetBoard = function (e){
-    //
-    // };
-
-    // var resetCards = function (){
-    //         console.log("Reset cards please");
-    //
-    //       };
-
-
-
-
-    //  div.addEventListener('click', isMatch);
-     div.addEventListener('click', turnCard);
-     div.addEventListener('click', isTwoCards);
+    div.addEventListener('click', turnCard);
+    div.addEventListener('click', isTwoCards);
   };
-
 };
 
 
 createBoard (cards);
-
-// var resetBoard = function (e) {
-//   console.log('start over');
-//   alert ("Let's start over")
-//   var puppy = document.querySelectorAll('.card img');
-//   for (var i= 0; i<cards.length; i++){
-//     puppy[i].style.visiblity= "hidden"
-//   };
-// };
-
-$("button").click(function(){
-  $("div").math.random("card img")
-});
