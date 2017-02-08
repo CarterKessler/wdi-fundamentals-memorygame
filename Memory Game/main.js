450f0e634dba5190a5d330d301c5bc6d8a6f35ff
Carter Kessler <rckessler84@yahoo.com>
2017-02-07 19:41:56 -0600

    added reset button logic

--- Memory Game/main.js
+++ Memory Game/main.js
@@ -1,42 +1,39 @@
-// var cards = ["jack", "king", "jack", "queen", "king", "queen"];
+
 var cardsInPlay = [];
 var cards = [];
-// // // var HBO = cards.length;
-// // // var face = "div";
+var jerk = [];
 var makeCardsArray = function(){
   var taco = [];
-
-      taco.push('jack');
-      taco.push('jack');
       taco.push('king');
+      taco.push('jack');
+      taco.push('joker');
       taco.push('king');
       taco.push('queen');
+      taco.push('jack');
       taco.push('queen');
       taco.push('joker');
-      taco.push('joker');
-
       return taco;
+    };
 
-};
-//
 cards = makeCardsArray();
-// [1, 7].fill("king");
-// cards.forEach(makeCardsArray(0, [0])"queen");
 
 console.log(cards);
 
-var randomCards= function() {
-  cards.sort(function(a, b){return 0.5 - Math.random()});
+var randomCards = function() {
+  if (jerk.length==4) {cards.sort(function(a, b){return 0.5 - Math.random()});
   createBoard(cards);
   console.log(cards);
+  jerk= [];
+  } else {
+    alert ("Finish this one first")
+  }
+
   };
-
-
 
 var turnCard = function(e){
   console.log(e.target.id);
   console.log(e.target.className);
-  // console.log(cards);
+  console.log(cards);
   // console.log(e.target.card[i]);
 
    if (e.target.className === "card king") {
@@ -49,8 +46,7 @@ var turnCard = function(e){
    else if (e.target.className === "card joker"){
      e.target.innerHTML='<img src="joker.jpg" alt="Joker" />'
    }
-   //isTwoCards;
-   // isTwoCards(e);
+
   };
 
 var isMatch = function (e) {
@@ -68,19 +64,22 @@ var isMatch = function (e) {
          console.log(cardsInPlay[0]);
          if (cardsInPlay[0]==="jack"){
            $("div").remove(".jack");
+           jerk.push("jack");
            cardsInPlay = [];
          } else if (cardsInPlay[0]==="queen"){
            $("div").remove(".queen");
+           jerk.push("queen");
            cardsInPlay = [];
          } else if (cardsInPlay[0]==="king"){
            $("div").remove(".king");
+           jerk.push("king");
            cardsInPlay = [];
          } else if (cardsInPlay[0]==="joker"){
            $("div").remove(".joker");
-          //  console.log("french fries");
+           jerk.push("joker");
            cardsInPlay = [];
          }
-
+         console.log(jerk);
        };
       //  document.getElementById("game-text").innerHTML= "It's a match, find another one";
 
@@ -90,43 +89,13 @@ var isMatch = function (e) {
 
        turnOver = setTimeout(resetCards, 500)
        function resetCards(e) {
-        //  console.log(cardsInPlay[0], cardsInPlay[1])
          var el = document.querySelectorAll('.card img');
          for (var i=0; i<el.length; i++){
              el[i].style.visibility='hidden'
            }
            cardsInPlay = [];
        };
-      //  resetCards(e);
-      //  cardsInPlay = [1];
-      //  var myReset;
-       //
-      //  myReset = setTimeout(resetCards, 1100)
-      //  var resetCards = function (e){
-      //     //  console.log('reset the cards please');
-      //    var el = document.querySelectorAll('.card img');
-      //    for (var i=0; i<cardsInPlay.length; i++){
-      //        el[i].style.visibility='hidden'
-      //      };
-      //    };
-
-      //  document.getElementById("game-text").innerHTML= "Keep going";
-      //  var myTar;
-       //
-      //  myTar= setTimeout(resetCards, 1200)
-      //  function resetCards(e) {
-      //    console.log('reset the cards please');
-       //
-      //     for (var i=0; i<cardsInPlay.length; i++) {
-      //      var el = document.querySelectorAll('.card img');
-      //      el[i].style.visibility='hidden';
-      //    };
-      //  };
-
-      //  isTwoCards();
-      // cardsInPlay = [0];
      }
-        // cardsInPlay = [];
      };
 
 var isTwoCards = function (e){
@@ -136,8 +105,6 @@ var isTwoCards = function (e){
       isMatch(cardsInPlay);
     } else {
       console.log('click one more')
-      // isMatch(cardsInPlay);
-      // cardsInPlay = [3];
     }
     };
 
@@ -155,14 +122,6 @@ var createBoard = function(cards){
      div.setAttribute('id', i);
      //add the div to the body, the game-board id
      document.getElementById("game-board").appendChild(div);
-
-  //  var resetCards = function (e){
-  //          console.log('reset the cards please');
-  //        var el = document.querySelectorAll('.card img');
-  //        for (var i=0; i<cardsInPlay.length; i++){
-  //            el[i].style.visibility='hidden'
-  //          };
-  //        };
 
     div.addEventListener('click', turnCard);
     div.addEventListener('click', isTwoCards);
